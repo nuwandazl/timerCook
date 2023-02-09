76c6ed6717e8df4c3c18d61ea187c7db44068e32
Zlata <repinazlata@mail.ru>
2023-02-09 10:54:18 +0500

    Добавление toolbar и главной страницы с рецептами, отображение главной страницы с рецептами под toolbar

--- app/src/main/java/com/example/timercook/MainActivity.java
+++ app/src/main/java/com/example/timercook/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecipesFragment fragment_recipes = new RecipesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_recipes).commit();
     }
 }
